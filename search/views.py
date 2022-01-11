46a7d58b2f449d86945f876b1598a4f7fd746ea4
Ranjeet Singh <techonerd1.0@gmail.com>
2022-01-12 02:23:08 +0530

    view to return search result

--- search/views.py
+++ search/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
-# from .models import *
-# Create your views here.
+from pymongo import MongoClient
+import os
+
+connect_string = os.environ.get('MONGODB_URI')
+cluster = MongoClient(connect_string)
+db = cluster["techonerd"]
+collection = db["banklist"]
+
 def index(request):
     return render(request, 'search/index.html')
 
 def search(request):
-    if request.method == 'POST':
-        query = request.POST['searched']
-        return render(request, 'search/search.html', {'query': query})
-    else:
+
+    if request.method != 'POST':
         return render(request, 'search/search.html', {})
+    q = request.POST['searched']
+    results = collection.find({'ifsc':q.upper()})
+    data={}
+    result = list(results)
+    if not result:
+        data["oh-no!!"]="No results found"
+    else:
+        for k,v in result[0].items():
+            if k in ['ifsc','branch','address','city','district','state','bank_name']:
+                data[k.upper()]= v
+    return render(request, 'search/search.html', {'query': data})
