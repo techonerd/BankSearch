76f3d9a5b2642e13bc597c88329b70dabed02f02
Ranjeet Singh <techonerd1.0@gmail.com>
2022-01-11 20:26:40 +0530

    UI for search

--- search/views.py
+++ search/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+# from .models import *
 # Create your views here.
+def index(request):
+    return render(request, 'search/index.html')
+
+def search(request):
+    if request.method == 'POST':
+        query = request.POST['searched']
+        return render(request, 'search/search.html', {'query': query})
+    else:
+        return render(request, 'search/search.html', {})
